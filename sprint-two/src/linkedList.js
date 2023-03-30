7689fecbe5eb815fedbdcd5374bb9cd073647872
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-03-29 20:09:32 -0700

    LinkedList complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if (list.head === null) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var value = list.head.value;
+    //assume list is Linkedlist
+    list.head = list.head.next;
+    // list.value = list.next.value;
+    // list.next = list.next.next;
+
+    return value;
   };
 
   list.contains = function(target) {
-  };
+    // if (list.head.value === target) {
+    //   return true;
+    // } else {
+    //   list.head.next.contains(target);
+    // }
 
+    // return false;
+
+    let currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  };
   return list;
 };
 
